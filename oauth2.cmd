2e070ccecbf613c0711d443f00bf31f33658873e
Student <Student>
2016-06-24 11:02:34 +0500

    v4a oauth2 running ok bug fixed

--- oauth2.cmd
+++ oauth2.cmd
@@ -1,2 +1,2 @@
-curl http://localhost:9000/greeting
-curl http://localhost:9000/greeting -H "Authorization: Bearer 000fe74a-98a7-4454-8f59-9110ce585c80"
+curl http://localhost:9000/users
+curl http://localhost:9000/users -H "Authorization: Bearer 000fe74a-98a7-4454-8f59-9110ce585c80"
